d18d9a115b1a80b1654784d46e162eb339f116a3
nito <b150005@outlook.jp>
2021-11-18 19:33:33 +0900

    Initial Commit

---
+++ CustomizableTimer/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CustomizableTimer
+//
+//  Created by 伊藤 直輝 on 2021/11/18.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
